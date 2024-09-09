1075107370d4023b9b0807fa91348da3c8ccd869
dghusername <mail@dgh.g66.org>
2024-09-09 10:28:30 +0000

    remove mqtt

--- dummycloud/app.js
+++ dummycloud/app.js
@@ -1,13 +1,13 @@
 const DummyCloud = require("./src/DummyCloud");
 const Logger = require("./src/Logger");
-const MqttClient = require("./src/MqttClient");
+//const MqttClient = require("./src/MqttClient");
 
 if (process.env.LOGLEVEL) {
     Logger.setLogLevel(process.env.LOGLEVEL);
 }
 
 const dummyCloud = new DummyCloud();
-const mqttClient = new MqttClient(dummyCloud);
+//const mqttClient = new MqttClient(dummyCloud);
 
 dummyCloud.initialize();
-mqttClient.initialize();
+//mqttClient.initialize();
